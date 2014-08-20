47d42a2422b1640abe6dc9db8310d2699baad145
Masaki Matsushita <glass.saga@gmail.com>
2014-08-20 21:57:04 +0900

    spec

--- spec/fiemap_spec.rb
+++ spec/fiemap_spec.rb
@@ -1,14 +1,53 @@
-decribe File::Extent do
-  before do
-    @t = Tempfile.new("fiemap", "w")    
-    @t.write("a" * 4096)
+require_relative "spec_helper"
+
+describe "File#extents" do
+  context "one extent" do
+    let(:tempfile) { Tempfile.new("fiemap_spec") }
+
+    before { tempfile.write("a" * 4096) }
+
+    subject { tempfile.extents }
+
+    it { is_expected.to be_a Array }
+    it { expect(subject.size).to eq(1) }
+
+    after do
+      tempfile.close unless tempfile.closed?
+    end
   end
 
-  it do
-    p @t
+  context "two extent" do
+    let(:tempfile) { Tempfile.new("fiemap_spec") }
+
+    before do
+      tempfile.write("a" * 4096)
+      tempfile.seek(4096, :CUR)
+      tempfile.write("a" * 4096)
+    end
+
+    subject { tempfile.extents }
+
+    it { p subject }
+    it { is_expected.to be_a Array }
+    it { expect(subject.size).to eq(2) }
+
+    after do
+      tempfile.close unless tempfile.closed?
+    end
   end
+end
+
+describe File::Extent do
+  let(:tempfile) { Tempfile.new("fiemap_spec") }
+
+  before { tempfile.write("a" * 4096) }
+
+  subject { tempfile.extents }
+
+  it { is_expected.to be_a Array }
+  it { expect(subject.size).to eq(1) }
 
   after do
-    @t.close unless t.closed!
+    tempfile.close unless tempfile.closed?
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,2 @@
 require "tempfile"
+require_relative "../lib/fiemap"
