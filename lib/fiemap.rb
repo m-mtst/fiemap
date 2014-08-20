9acaada37bbd942b3fdd4dad39ada613350d5c8e
Masaki Matsushita <glass.saga@gmail.com>
2014-08-20 21:56:49 +0900

    improve debug print

--- lib/fiemap.rb
+++ lib/fiemap.rb
@@ -4,5 +4,25 @@ require "fiemap/fiemap"
 class File
   class Extent
     private_class_method :new
+
+    def inspect
+      "#<#{self.class} logical:#{self.offset} physical:#{self.physical_offset} flags:#{flags}>"
+    end
+
+    private
+
+    def flags
+      flags = []
+      flags << "last" if last?
+      flags << "delalloc" if delalloc?
+      flags << "encoded" if encoded?
+      flags << "encrypted" if encrypted?
+      flags << "not_aligned" if not_aligned?
+      flags << "inline?" if inline?
+      flags << "tail" if tail?
+      flags << "unwritten" if unwritten?
+      flags << "merged" if merged?
+      flags.join(",")
+    end
   end
 end
